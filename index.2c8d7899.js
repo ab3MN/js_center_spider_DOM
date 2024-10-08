2392777d15616e254a48396c6deb71f0af3077b3
ab3MN <ab3mn3@gmail.com>
2024-10-08 09:59:59 +0300

    make build

---
+++ index.2c8d7899.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.left=(t.clientWidth-e.width)/2+"px",e.style.top=(t.clientHeight-e.height)/2+"px";
+//# sourceMappingURL=index.2c8d7899.js.map
